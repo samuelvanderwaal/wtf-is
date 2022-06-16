23c06531f44caf0e85f4a80ef0438a3c130b8151
Samuel Vanderwaal <samuel.vanderwaal@gmail.com>
2022-06-15 22:13:58 -0800

    refactor: refactor to match metaboss changes

--- README.md
+++ README.md
@@ -3,13 +3,19 @@
 I was tired of looking up Metaplex program errors.
 
 ```bash
-wtf-is <hex_code_no_prefix>
+wtf-is <hex_code || decimal_code>
 ```
 
 Example:
 
 ```bash
-$ wtf-is 37
+$ wtf-is 0x37
+> 0x37: 
+        Token Metadata            |     CannotUnverifyAnotherCreator: You cannot unilaterally unverify another creator
+```
+
+```bash
+$ wtf-is 55
 > 0x37: 
         Token Metadata            |     CannotUnverifyAnotherCreator: You cannot unilaterally unverify another creator
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::env::args;
 
+use errors::*;
+
 mod errors;
 
 pub struct FoundError {
@@ -8,63 +10,61 @@ pub struct FoundError {
 }
 
 fn main() {
-    let hex_code = args().nth(1);
+    let error_code = args().nth(1).unwrap();
 
-    match hex_code {
-        Some(code) => {
-            let code = code.to_uppercase();
-            let errors = find_errors(&code);
+    let parsed_error_code = if error_code.contains("0x") {
+        error_code.replace("0x", "")
+    } else {
+        format!("{:X}", error_code.parse::<i64>().unwrap())
+    };
 
-            if errors.is_empty() {
-                println!("No errors for found code: {}", code);
-                std::process::exit(0);
-            }
-            println!("0x{}: ", code);
-            for error in errors {
-                println!("\t{:<25} |\t{}", error.domain, error.message);
-            }
-        }
-        None => {
-            println!("No hex code provided!");
-            std::process::exit(1);
-        }
+    let errors = find_errors(&parsed_error_code);
+
+    if errors.is_empty() {
+        println!("No errors for found code: {parsed_error_code}");
+        std::process::exit(1);
+    }
+
+    println!("0x{}: ", parsed_error_code);
+    for error in errors {
+        println!("\t{:<25} |\t{}", error.domain, error.message);
     }
 }
 
 pub fn find_errors(hex_code: &str) -> Vec<FoundError> {
     let mut found_errors: Vec<FoundError> = Vec::new();
 
-    if let Some(e) = errors::ANCHOR_PROGRAM.get(hex_code).cloned() {
+    if let Some(e) = ANCHOR_ERROR.get(hex_code).cloned() {
         found_errors.push(FoundError {
             domain: "Anchor Program".to_string(),
             message: e.to_string(),
         });
     }
 
-    if let Some(e) = errors::AUCTION_HOUSE.get(hex_code).cloned() {
+    if let Some(e) = METADATA_ERROR.get(hex_code).cloned() {
+        found_errors.push(FoundError {
+            domain: "Token Metadata".to_string(),
+            message: e.to_string(),
+        });
+    }
+
+    if let Some(e) = AUCTION_HOUSE_ERROR.get(hex_code).cloned() {
         found_errors.push(FoundError {
             domain: "Auction House".to_string(),
             message: e.to_string(),
         });
     }
 
-    if let Some(e) = errors::NFT_CANDY_MACHINE.get(hex_code).cloned() {
+    if let Some(e) = AUCTIONEER_ERROR.get(hex_code).cloned() {
         found_errors.push(FoundError {
-            domain: "NFT Candy Machine V1".to_string(),
+            domain: "Auctioneer".to_string(),
             message: e.to_string(),
         });
     }
 
-    if let Some(e) = errors::NFT_CANDY_MACHINE_V2.get(hex_code).cloned() {
+    if let Some(e) = CANDY_ERROR.get(hex_code).cloned() {
         found_errors.push(FoundError {
-            domain: "NFT Candy Machine V2".to_string(),
-            message: e.to_string(),
-        });
-    }
-
-    if let Some(e) = errors::TOKEN_METADATA.get(hex_code).cloned() {
-        found_errors.push(FoundError {
-            domain: "Token Metadata".to_string(),
+            domain: "Candy Machine".to_string(),
             message: e.to_string(),
         });
     }
